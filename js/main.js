55e0cb20f20eb856d71a0b281adce217f71b8be9
Andrew Clark <andrewscomputer@Andrews-MacBook-Air.local>
2020-02-12 15:48:04 -0700

    total rework with new plan  v1

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let deck = [];
 let dealersHand = [];
 let playersHand = [];
 
+
 let players = []
 function createPlayers(num)
 {
@@ -17,92 +18,146 @@ function createPlayers(num)
 }
 
 
-function getDeck() {
+function Deck() {
 
-	for (let i = 0; i < suits.length; i++) {
-		for (let x = 0; x < cards.length; x++) {
-            let weight = parseInt(cards[x]);
-            if (cards[x] == "J" || cards[x] == "Q" || cards[x] == "K")
-                weight = 10;
-            if (cards[x] == "A")
-                weight = 11;
-            let cardName = (cards[x] + ` of ` + suits[i] + ` ` + weight);
-            const suitLetter = suits[i][0].toUpperCase();
-            const card = {
-                name: cards[x] + ` of ` + suits[i],
-                weight: weight,
-                fileName: `${suits[i] + cards[x]}.svg`
-            }
-            console.log(card);
-            deck.push(card);
-            
-		}
-	}
-
-	return deck;
+    this.deck = new array (52);
+    this.count = 52;
+    var c = 0;
+    for (var i = 1; i <= 4; i++)
+        for (var j = 1; j<= 13; j++)
+            this.deck[c++] = new card(j, i);
 }
 
 function load()
 {
-    getDeck();
+    Deck();
+    
 }
 
 window.onload = load;
 
 let init = function() {
     deck = [];
-    getDeck();
+    Deck();
     playersHand = [];
     dealersHand = [];
 }
 
-document.querySelector('#start').addEventListener('click', function (e) {
+
+
+
+//////////////////
+
+function Card(value, suit) {
+    this.suit = -1;
+    this.value = -1;
+
+    if (arguments.length >= 2)
+    this.set(arguments[0], arguments[1]);
+}
+
+card.ace = 1;
+card.jack = 11;
+card.queen = 12;
+card.king = 13;
+
+
+card.club = 1;
+card.diamond = 2;
+card.spade = 4;
+card.heart = 3;
+
+card.clear = function() {
+    this.suit = -1;
+    this.value = -1;
+}
+
+card.set = function (value, suit) {
+    if (arguments.length < 2)
+        alert('the set function requires two args');
+       var v = Math.round(Number(value));
+        var s = Math.round(Number(suit));
+        if ( ! (v > -1 && v<=13) ) 
+            alert('the value of a card must be in the range 1 to 13');
+        if ( ! (s >= 1 && s<= 4) )
+            alert('the suit of a card must be 1,2,3, or 4');
+        this.suit = s;
+        this.value = v;
+
+}
+
+card.toString = function() {
+    if (this.value == -1)
+        return "(Card not shown)";
+    var s = "";
+    switch (this.value) {
+        case 1: s += "Ace"; break;
+        case 11:s += "Jack"; break;
+        case 12: s+= "Queen"; break;
+        case 13: s+= "King"; break;
+        derfault: s  += this.value; break;
+}
+
+
+s += " of ";
+switch (this.suit) {
+    case 1: s += "Clubs"; break;
+    case 2: s += "Diamonds"; break;
+    case 3: s += "Hearts"; break;
+    case 4: s += "Spades; break";
+}
+return s;
+
+deck.shuffle = function() {
+    for (var i = 51; i > 0; i--) {
+        var r = Math.floor((i+1) * Math.random(i));
+        var temp = this.deck(r);
+        this.deck(r) = this.deck[i];
+        this.deck[i] = temp;
+    }
+    this.count = 52;
+}
+
+let nextCard = function() {
+    if (this.count == 0)
+        alert ("Deck is out of cards");
+    return this.deck[--this.count];
+}
+
+
+
+
+document.querySelector('#deal').addEventListener('click', function (e) {
     init();
     getDealer();
     getDealer();
-    getPlayer();
     getPlayer();
     
 })
 
 let getDealer = function() {
     let playCard = deck[Math.floor(Math.random() * deck.length)];
-        dealersHand.push(playCard.name);
+        dealersHand.push(playCard);
         console.log(dealersHand);
 }
 let getPlayer = function() {
     let playCard = deck[Math.floor(Math.random() * deck.length)];
-        playersHand.push(playCard.name)
-        let first = document.querySelector('.test');
-        first.innerHTML = playersHand;
+    let playCard2 = deck[Math.floor(Math.random() * 30)];
+        playersHand.push(playCard, playCard2)
+        let title = document.querySelector('.test');
+        title.innerHTML = playersHand[0].name + ` ` + playersHand[1].name + ` `;
         console.log(playersHand)
 }
+let hit = function() {
+    let playersHand = [];
+    let playCard3 = deck[Math.floor(Math.random * 29)];
+    playersHand.push(playCard3);
 
-document.querySelector('#hit').addEventListener('click', getPlayer);
-
-// let checkOver = function() {
-//     if playCard.weight
-// }
-
-function createComputer() {
-    document.getElementById('dealersCards').innerHTML = '';
-    for (var i = 0; i < players.length; i++)
-    {
-        let div_player = document.createElement('div');
-        let div_playerid = document.createElement('div');
-        let div_hand = document.createElement('div');
-        let div_points = document.createElement('div');
-        
-        div_points.className = 'points';
-        div_points.id = 'points_' + i;
-        div_plsyer.id ='player';
-        div_hand.id ='hand_' + i;
-
-        div_playerid.innerHTML = players[i].ID;
-        div_player.appendChild(div_playerid);
-        div_player.appendChild(div_hand);
-        div_player.appendChild(div_points);
-        document.getElementById('.test').appendChild(div_player);
-    }
-
+    if (playersHand[0].weight + playersHand[1].weight === 21 || PlayersHand[0].weight + playersHand[1].weight + playersHand[2].weight === 21) {
+        document.querySelector('.test').innerHTML = 'Player Busted!'
+    };
 }
+
+document.querySelector('#hit').addEventListener('click', hit);
+
+
